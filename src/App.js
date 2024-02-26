22dd1293175d91abda258e10798fe7ca0be84005
Егор Сухоруков <SEM200107@mail.ru>
2024-02-26 11:18:55 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        asd
-      </div>
       <Routes>
         <Route path={'selectDate'} element={<SelectDate />} />
       </Routes>
