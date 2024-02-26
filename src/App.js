7c889af58b0abcf70115a7a999a813b944c51526
Егор Сухоруков <SEM200107@mail.ru>
2024-02-26 10:19:44 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
+import {Route, Routers} from 'react-router-dom';
+import SelectDate from './components/SelectDate/SelectDate';
 
 function App() {
-  const {tg} = useTelegram();
-  const [date, setDate] = useState(undefined);
-
-  useEffect(() => {
-    tg.ready();
-    tg.MainButton.setParams({
-      text: 'Отправить'
-    });
-  }, [tg]);
-
-  const onSendData = useCallback(() => {
-    const data = date;
-    tg.sendData(data);
-  }, [tg, date]);
-
-  useEffect(() => {
-    tg.onEvent('mainButtonClicked', onSendData);
-    return () => {
-      tg.offEvent('mainButtonClicked', onSendData);
-    };
-  }, [tg, onSendData]);
-
-  const onClickToday = () => {
-    const today = new Date().toISOString().slice(0, 10);
-    setDate(today);
-    tg.MainButton.show();
-  };
-
-  const onClickYesterday = () => {
-    const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
-    setDate(yesterday);
-    tg.MainButton.show();
-  };
 
   return (
     <div className="App">
-      <div>
-        <div>
-        <button className='buttonYesterday' onClick={onClickYesterday}>Вчера</button>
-        <button className='buttonToday' onClick={onClickToday}>Сегодня</button>
-        </div>
-        <input
-          type='date'
-          value={date}
-          onChange={e => { setDate(e.target.value); tg.MainButton.show(); }}
-        />
-      </div>
+      <Routers>
+        <Route path={'selectDate'} element={<SelectDate />} />
+      </Routers>
     </div>
   );
 }
---
+++ src/components/SelectDate/SelectDate.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import './SelectDate.css';
+import { useTelegram } from './hooks/useTelegram';
+
+const SelectDate = () => {
+
+    const {tg} = useTelegram();
+    const [date, setDate] = useState(undefined);
+
+    useEffect(() => {
+    tg.ready();
+    tg.MainButton.setParams({
+        text: 'Отправить'
+    });
+    }, [tg]);
+
+    const onSendData = useCallback(() => {
+    const data = date;
+    tg.sendData(data);
+    }, [tg, date]);
+
+    useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+        tg.offEvent('mainButtonClicked', onSendData);
+    };
+    }, [tg, onSendData]);
+
+    const onClickToday = () => {
+    const today = new Date().toISOString().slice(0, 10);
+    setDate(today);
+    tg.MainButton.show();
+    };
+
+    const onClickYesterday = () => {
+    const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
+    setDate(yesterday);
+    tg.MainButton.show();
+    };
+    
+    return(
+        <div className='selectDate' >
+            <div>
+                <button className='buttonYesterday' onClick={onClickYesterday}>Вчера</button>
+                <button className='buttonToday' onClick={onClickToday}>Сегодня</button>
+            </div>
+                <input
+                type='date'
+                value={date}
+                onChange={e => { setDate(e.target.value); tg.MainButton.show(); }}
+                />
+        </div>
+    );
+};
+
+export default SelectDate;
+
