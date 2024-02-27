53d2b883d78a2b3a54d769782d796c9fa74534ba
Егор Сухоруков <SEM200107@mail.ru>
2024-02-27 11:42:52 +0300

    asd

--- src/components/SelectDate/SelectDate.jsx
+++ src/components/SelectDate/SelectDate.jsx
@@ -3,15 +3,14 @@ import './SelectDate.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
 const SelectDate = () => {
-
-    const {tg} = useTelegram();
+    const { tg } = useTelegram();
     const [date, setDate] = useState(undefined);
 
     useEffect(() => {
-    tg.ready();
-    tg.MainButton.setParams({
-        text: 'Отправить'
-    });
+        tg.ready();
+        tg.MainButton.setParams({
+            text: 'Отправить'
+        });
     }, [tg]);
 
     const onSendData = useCallback(() => {
@@ -19,36 +18,63 @@ const SelectDate = () => {
     }, [tg, date]);
 
     useEffect(() => {
-    tg.onEvent('mainButtonClicked', onSendData);
-    return () => {
-        tg.offEvent('mainButtonClicked', onSendData);
-    };
+        tg.onEvent('mainButtonClicked', onSendData);
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData);
+        };
     }, [tg, onSendData]);
 
-    const onClickToday = () => {
-    const today = new Date().toISOString().slice(0, 10);
-    setDate(today);
-    tg.MainButton.show();
+    const generateDates = () => {
+        const dates = [];
+        for (let i = 7; i > 1; i--) {
+            const today = new Date();
+            const fiveDaysAgo = new Date(today);
+            fiveDaysAgo.setDate(today.getDate() - i);
+            dates.push(fiveDaysAgo);  
+        }
+        return dates;
     };
 
-    const onClickYesterday = () => {
-    const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
-    setDate(yesterday);
-    tg.MainButton.show();
-    };
+    const dateToString = (date) => {
+        const month = String(date.getMonth() + 1).padStart(2, '0'); 
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${day}.${month}`;
+    }
+
+    const dates = generateDates();
+
+    const onClickToday = () => {
+        const today = new Date().toISOString().slice(0, 10);
+        setDate(today);
+        tg.MainButton.show();
+        };
     
-    return(
+    const onClickYesterday = () => {
+        const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
+        setDate(yesterday);
+        tg.MainButton.show();
+        };
+
+    return (
         <div className='selectDate'>
-            <button className='buttonYesterday' onClick={onClickYesterday}>Вчера</button>
             <input
                 type='date'
                 value={date}
-                onChange={e => { setDate(e.target.value); tg.MainButton.show(); }}
+                onChange={(e) => { setDate(e.target.value); tg.MainButton.show(); }}
             />
-            <button className='buttonToday' onClick={onClickToday}>Сегодня</button>
+            <div className='days-btn'>
+                 {dates.map((date) => {
+                     return (
+                         <button className='day-btn' onClick={() => setDate(date.toISOString().slice(0, 10))}>
+                             {dateToString(date)}
+                         </button>
+                     );
+                 })}
+                 <button className='buttonYesterday' onClick={onClickYesterday}>Вчера</button>
+                 <button className='buttonToday' onClick={onClickToday}>Сегодня</button>
+             </div>
         </div>
     );
 };
 
 export default SelectDate;
-
