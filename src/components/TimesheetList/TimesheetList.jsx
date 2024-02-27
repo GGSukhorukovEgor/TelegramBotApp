1d15617d1bfe87da2a9e8b011dc2bb663be0ca4f
Егор Сухоруков <SEM200107@mail.ru>
2024-02-27 12:40:42 +0300

    фыв

--- src/components/TimesheetList/TimesheetList.jsx
+++ src/components/TimesheetList/TimesheetList.jsx
@@ -1,34 +1,50 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import TimesheetItem from '../TimesheetItem/TimesheetItem'
+import TimesheetItem from '../TimesheetItem/TimesheetItem';
 import './TimesheetList.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
 const TimesheetList = () => {
-
-    const {tg} = useTelegram();
+    const { tg } = useTelegram();
     const [searchParams, setSearchParams] = useSearchParams();
     const [timesheetList, setTimesheetList] = useState(undefined);
+    const [selectedTimesheet, setSelectedTimesheet] = useState(null);
 
     useEffect(() => {
         let timesheetsJson = atob(searchParams.get("data"));
         setTimesheetList(JSON.parse(timesheetsJson));
-        }, [setTimesheetList, setSearchParams, searchParams]);
+    }, [setTimesheetList, setSearchParams, searchParams]);
+
+    const confirmAndSendData = (timesheet) => {
+        const isConfirmed = window.confirm('Вы уверены, что хотиде удалить timesheet?');
+
+        if (isConfirmed) {
+            tg.sendData(JSON.stringify(timesheet));
+        }
+    };
 
     const onSelect = (timesheet) => {
-        tg.sendData(JSON.stringify(timesheet));
-    }
+        setSelectedTimesheet(timesheet);
+    };
 
-    return(
+    useEffect(() => {
+        if (selectedTimesheet) {
+            confirmAndSendData(selectedTimesheet);
+            setSelectedTimesheet(null); // Reset selectedTimesheet after sending data
+        }
+    }, [selectedTimesheet]);
+
+    return (
         <div className='timesheet_list'>
             {timesheetList && timesheetList.map(item => (
-                <TimesheetItem 
-                    timesheet={item} 
+                <TimesheetItem
+                    key={item.id}
+                    timesheet={item}
                     onSelect={onSelect}
                 />
             ))}
         </div>
-    )
-}
+    );
+};
 
 export default TimesheetList;
